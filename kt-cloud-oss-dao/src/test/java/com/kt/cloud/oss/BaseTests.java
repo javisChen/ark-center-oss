875853d84a52fc9caaf92611f3c49ec74320d1fc
Javis <javischen9548@foxmail.com>
2022-02-28 12:33:48 +0800

    first commit

---
+++ kt-cloud-oss-dao/src/test/java/com/kt/cloud/oss/BaseTests.java
@@ -0,0 +1,15 @@
+package com.kt.cloud.oss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+public class BaseTests {
+
+    @Test
+    public void test() {
+
+    }
+}

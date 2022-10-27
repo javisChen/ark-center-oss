d37fefb0f231c1d723691a4a45be47ec30110b9f
Javis <javischen9548@foxmail.com>
2022-10-27 17:55:49 +0800

    u

--- ark-center-oss-start/src/main/java/com/ark/center/oss/Application.java
+++ ark-center-oss-start/src/main/java/com/ark/center/oss/Application.java
@@ -1,6 +1,6 @@
 package com.ark.center.oss;
 
-import com.ark.component.web.config.CloudAppConfig;
+import com.ark.component.web.config.ArkWebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication()
 @EnableFeignClients()
 @EnableDiscoveryClient
-public class Application  extends CloudAppConfig {
+public class Application extends ArkWebConfig {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

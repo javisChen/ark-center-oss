ccbf33bc2f39cc3ff0dadd40989463fcceb95f5a
Javis <javischen9548@foxmail.com>
2022-10-27 23:28:00 +0800

    u

--- ark-center-oss-start/src/main/java/com/ark/center/oss/Application.java
+++ ark-center-oss-start/src/main/java/com/ark/center/oss/Application.java
@@ -2,11 +2,14 @@ package com.ark.center.oss;
 
 import com.ark.component.web.config.ArkWebConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication()
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableFeignClients()
 @EnableDiscoveryClient
 public class Application extends ArkWebConfig {

a153b0b426cfb1b74b05a25ad6420866d9f641bf
chengmo.cjw <javischen9548@foxmail.com>
2024-08-14 15:45:55 +0800

    u

--- ark-center-oss-service/src/main/java/com/ark/center/oss/module/core/service/OssService.java
+++ ark-center-oss-service/src/main/java/com/ark/center/oss/module/core/service/OssService.java
@@ -1,29 +1,40 @@
 package com.ark.center.oss.module.core.service;
 
+import cn.hutool.core.collection.CollUtil;
 import com.ark.center.oss.module.core.dto.OssUploadReqDTO;
 import com.ark.center.oss.module.core.constants.OssTypeEnums;
 import com.ark.center.oss.module.core.dto.OssUploadRespDTO;
 import com.ark.component.exception.ExceptionFactory;
-import com.ark.component.oss.IObjectStorageService;
+import com.ark.component.oss.OssStrategy;
+import com.ark.component.oss.OssType;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 @Slf4j
 public class OssService {
 
-    private final Map<String, IObjectStorageService> objectStorageServiceMap;
+    private Map<OssType, OssStrategy> strategyHolder;
 
-    public OssService(Map<String, IObjectStorageService> objectStorageServiceMap) {
-        this.objectStorageServiceMap = objectStorageServiceMap;
+
+    public OssService(List<OssStrategy> ossStrategies) {
+        if (CollUtil.isNotEmpty(ossStrategies)) {
+            strategyHolder = new HashMap<>(ossStrategies.size());
+            for (OssStrategy ossStrategy : ossStrategies) {
+                strategyHolder.put(ossStrategy.ossType(), ossStrategy);
+            }
+        }
     }
 
     public OssUploadRespDTO upload(OssUploadReqDTO ossUploadDTO) {
         checkBeforeUpload(ossUploadDTO);
-        IObjectStorageService storageService = getStorageService(ossUploadDTO);
+        OssStrategy storageService = getStrategy(ossUploadDTO);
         if (storageService == null) {
             throw ExceptionFactory.userException("无效的OSS类型");
         }
@@ -47,14 +58,9 @@ public class OssService {
         }
     }
 
-    private IObjectStorageService getStorageService(OssUploadReqDTO ossUploadDTO) {
+    private OssStrategy getStrategy(OssUploadReqDTO ossUploadDTO) {
         String type = ossUploadDTO.getType();
-        if (type.equals(OssTypeEnums.MINIO.getValue())) {
-            return objectStorageServiceMap.get("minIoOssObjectStorageService");
-        } else if (type.equals(OssTypeEnums.ALIYUN.getValue())) {
-            return objectStorageServiceMap.get("aliYunOssObjectStorageService");
-        }
-        return null;
+        return strategyHolder.get(OssType.valueOf(type));
     }
 
     private OssUploadRespDTO toOssUploadRespDTO(String ossUrl) {
